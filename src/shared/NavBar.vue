56373361a607421bd3217b60b776c05abe8e954d
stevensewalu <ssewalu@projectcode.ug>
2023-06-06 12:36:55 +0300

    inital template

---
+++ src/shared/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <div class="bg-amber-950 w-full text-white p-4 shadow">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import {RouterLink} from "vue-router";
+</script>
+
+<style scoped>
+
+</style>
